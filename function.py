9ac46f85b512595ea2601d764425697de3a48c5a
Sumit Goswami <sumitgoswami8092@gmail.com>
2026-02-10 18:58:21 +0530

    practice of function and recursion

---
+++ function.py
@@ -0,0 +1,12 @@
+#function definition
+def avg():
+    a = int(input("Enter The number: "))
+    b = int(input("Enter the number: "))
+    c = int(input("Enter the number: "))
+    print((a+b+c)/3)
+#function call
+avg()
+avg()
+avg()
+avg()
+avg()
---
+++ function_with_argument.py
@@ -0,0 +1,9 @@
+def goodDay(name,ending):
+    print("Good Day, " + name)
+    print(ending)
+
+goodDay("harry", "Thank you")
+goodDay("sumit", "Thank you")
+goodDay("rohan", "Thank you")
+goodDay("vikash", "Thank you")
+
