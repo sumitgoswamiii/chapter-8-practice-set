bb8a7167c05fd003e6a64356d58a93244006ba50
Sumit Goswami <sumitgoswami8092@gmail.com>
2026-02-16 18:25:06 +0530

    practice of if and else

---
+++ python by Hitesh sir/snake_suggestion.py
@@ -0,0 +1,8 @@
+snack = input("Enter your preffered snack: ").lower()
+
+if(snack== "cookie" or snack == "samosa"):
+    print(f"Great choice! we'll serve you:  {snack}")
+
+else:
+    
+    print("Sorry,we only serve cookie or samosa with tea")
