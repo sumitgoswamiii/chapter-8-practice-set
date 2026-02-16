141b58ea857b564add4ac11ef9c5ee690f5b3ad6
Sumit Goswami <sumitgoswami8092@gmail.com>
2026-02-16 17:07:22 +0530

    practice of sets and dictionary

---
+++ python by Hitesh sir/quiz_practice.py
@@ -0,0 +1,16 @@
+a = "python"[0]
+print(a)
+b = "programming"[0:5]
+print(b)
+print(max([1,5,3]))
+
+c = "hello" +"world"
+print(c)
+dict = {
+    "name":"sumit",
+    "age" : "20",
+}
+print(dict.get("google"))
+
+
+
